e5a85bcac9bac92b3122b99190ac1c7d79d3ff36
Tieu Khe Mayer <tieukhe.mayer@gmail.com>
2020-03-06 11:52:17 +0100

    mixblendmode

--- main.js
+++ main.js
@@ -3,9 +3,16 @@ test123 = function() {
 player.play();
 
 // or load via a Bodymovin JSON string/object
-
-
   console.log("hallo")
 }
 
 h1.addEventListener("mouseover", test123)
+
+
+
+if('CSS' in window && 'supports' in window.CSS) {
+    var support = window.CSS.supports('mix-blend-mode','difference');
+  // tests for mix-blend-mode support
+    support = support?'mix-blend-mode':'no-mix-blend-mode';
+document.documentElement.className += support;
+}
