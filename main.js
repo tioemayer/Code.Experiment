21916b48770396b8a762b071b0173f9f5c6fd851
Tiö Mayer <31538441+tioemayer@users.noreply.github.com>
2020-03-06 08:41:06 +0100

    Add files

---
+++ main.js
@@ -0,0 +1,11 @@
+test123 = function() {
+  const player = document.querySelector('#heart');
+player.play();
+
+// or load via a Bodymovin JSON string/object
+
+
+  console.log("hallo")
+}
+
+h1.addEventListener("mouseover", test123)
